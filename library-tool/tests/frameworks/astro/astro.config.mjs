c30f4139560c0bcc0b2761826444024686765874
Joseth <jojo.programmer01@gmail.com>
2025-10-05 17:20:01 +0100

    fix: update Astro configuration and package files for improved server setup

--- library-tool/tests/frameworks/astro/astro.config.mjs
+++ library-tool/tests/frameworks/astro/astro.config.mjs
@@ -7,7 +7,16 @@ import vue from '@astrojs/vue';
 
 import svelte from '@astrojs/svelte';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  server: {
+    host: '0.0.0.0'
+  },
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [react(), vue(), svelte()]
 });
