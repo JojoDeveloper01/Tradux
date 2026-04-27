a2e708db0578865e12f26599563ffcc124dd1e8b
JojoDeveloper01 <jojoprofeammer01@gmail.com>
2026-04-27 21:28:53 +0100

    Refactor code structure for improved readability and maintainability

--- examples/astro/astro.config.mjs
+++ examples/astro/astro.config.mjs
@@ -2,9 +2,18 @@
 
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [sitemap()],
+
+  output: "server",
+  server: {
+    host: "0.0.0.0",
+  },
+  adapter: node({
+    mode: "standalone",
+  }),
 });
